131bda0654affb9d85333afda1f31c52af9b2065
naotoshi-ikeda-creater <nao.0728.2017@gmail.com>
2020-02-21 21:06:19 +0900

    branch test

---
+++ frontend/src/components/pages/tasklist.vue
@@ -0,0 +1,18 @@
+<template>
+    <v-card>
+        <v-toolbar-title> <!--　付け加える -->
+             <v-spacer></v-spacer>　<!--空間を開ける -->
+             <v-icon>]
+                 <!-- アイコンの設定 -->
+             </v-icon>
+        </v-toolbar-title>
+        <v-divider></v-divider> <!--線を入れる -->　　
+        <!-- ここまでがヘッダー  -->
+
+        
+
+
+
+
+    </v-card>
+</template>
